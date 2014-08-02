e0d034cf825ca0bb70f798d5c368bcdd5554133b
iiD4x <t2pksa@gmail.com>
2014-08-02 07:04:16 +0300

    GCC: Update.
    4.9 for Kernel.
    4.8 for ROM.
    
    Change-Id: Icb20c784ff3af50fbedc3787deb25219f6a30752

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,7 +8,8 @@ STE_HARDWARE:= true
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/samsung/u8500
 TARGET_KERNEL_CONFIG := cyanogenmod_i8160_defconfig
-ARM_EABI_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin
+TARGET_GCC_VERSION_ARM := 4.9-sm
+TARGET_GCC_VERSION_AND := 4.8-sm
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/codina/bluetooth
